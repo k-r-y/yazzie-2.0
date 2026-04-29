4d011d1147f16338eb08fff0030a6d3c5b2b0fc3
kry <casianoprince5@gmail.com>
2026-04-29 20:33:15 +0800

    security: add .gitignore and untrack sensitive files

--- scratch/describe_booking_dishes.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once __DIR__ . '/../config/config.php';
-$stmt = $pdo->query("DESCRIBE booking_dishes");
-$cols = $stmt->fetchAll();
-foreach ($cols as $c) {
-    echo $c['Field'] . " (" . $c['Type'] . ")\n";
-}
--- scratch/describe_custom_items.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once __DIR__ . '/../config/config.php';
-$stmt = $pdo->query("DESCRIBE booking_custom_items");
-$cols = $stmt->fetchAll();
-foreach ($cols as $c) {
-    echo $c['Field'] . " (" . $c['Type'] . ")\n";
-}
